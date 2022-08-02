92eb408356eca694a827ccfdd61b7ddf34b81297
Ken Chu <chuwingyu@gmail.com>
2022-08-03 02:16:55 +0800

    [doom-emacs] Try some emacs packages and config.

--- doom-emacs/.config/doom/init.el
+++ doom-emacs/.config/doom/init.el
@@ -36,8 +36,7 @@
        hl-todo           ; highlight TODO/FIXME/NOTE/DEPRECATED/HACK/REVIEW
        hydra
        indent-guides     ; highlighted indent columns
-       (ligatures +iosevka
-                  +extra) ; ligatures and symbols to make your code pretty again
+       (ligatures +hasklig +extra) ; ligatures and symbols to make your code pretty again
        minimap           ; show a map of the code on the side
        modeline          ; snazzy, Atom-inspired modeline, plus API
        nav-flash         ; blink cursor line after big motions
