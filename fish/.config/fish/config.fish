aa8e9be1e3544b00ea5db5b48f7d8438f670c0ed
Ken Chu <chuwingyu@gmail.com>
2022-08-10 20:02:15 +0800

    [fish] update config.fish

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,18 +1,18 @@
-# https://www.reddit.com/r/fishshell/comments/govh0k/recommended_way_of_setting_environment_vars_in/
-# set environment variables
-set -x XDG_CONFIG_HOME $HOME/.config
-
 # with both ampersand-nobg-in-token and qmark-noglob, this argument has no special characters anymore
 set -x fish_features ampersand-nobg-in-token qmark-noglob
 
-# set -x GOPATH $HOME/go
+# set environment variables
+# https://www.reddit.com/r/fishshell/comments/govh0k/recommended_way_of_setting_environment_vars_in/
+set -x XDG_CONFIG_HOME $HOME/.config
+set -x GOPATH $HOME/go
+set -x JAVA_HOME $HOME/.asdf/installs/java/microsoft-17.0.4
 
 # set fish_user_paths
 # fish_add_path /usr/local/bin
+fish_add_path $GOPATH/bin
 fish_add_path $HOME/.emacs.d/bin
 fish_add_path $HOME/.anyenv/bin
 fish_add_path $HOME/.cargo/bin
-# fish_add_path $HOME/.pyenv/bin
 
 # https://github.com/jimeh/emacs-builds
 if test -d /Applications/Emacs.app/Contents/MacOS/bin
