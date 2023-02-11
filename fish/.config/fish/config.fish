606480710e1eaaa3fd79cbc37ed71d233828bd10
Ken Chu <chuwingyu@gmail.com>
2023-02-11 20:34:37 +0800

    [fish] update brew prefix path for Mac M1 Pro

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,30 +1,7 @@
+# https://fishshell.com/docs/current/language.html
+set fish_features ampersand-nobg-in-token qmark-noglob
 # https://stackoverflow.com/questions/48956984/how-to-remap-escape-insert-mode-to-jk-in-fish-shell
 set fish_key_bindings fish_user_key_bindings
-
-# https://fishshell.com/docs/current/language.html
-set -x fish_features ampersand-nobg-in-token qmark-noglob
-
-# set environment variables
-# https://www.reddit.com/r/fishshell/comments/govh0k/recommended_way_of_setting_environment_vars_in/
-set -x XDG_CONFIG_HOME $HOME/.config
-#set -x GOPATH $HOME/go
-set -x JAVA_HOME $HOME/.asdf/installs/java/microsoft-17.0.5
-
-# set fish_user_paths
-# fish_add_path /usr/local/bin
-fish_add_path /usr/local/opt/uutils-coreutils/libexec/uubin
-fish_add_path $HOME/.asdf/shims
-fish_add_path $HOME/.anyenv/bin
-#fish_add_path $HOME/.cargo/bin
-#fish_add_path $GOPATH/bin
-fish_add_path $XDG_CONFIG_HOME/emacs/bin
-
-# https://github.com/jimeh/emacs-builds
-if test -d /Applications/Emacs.app/Contents/MacOS/bin
-    fish_add_path /Applications/Emacs.app/Contents/MacOS/bin
-    alias emacs="emacsclient -nw"
-end
-
 fish_vi_key_bindings
 
 # https://github.com/PatrickF1/fzf.fish/wiki/Migration-Guides#v7
@@ -39,6 +16,8 @@ end
 
 if status is-interactive
     # Commands to run in interactive sessions can go here
+    # source (/opt/homebrew/bin/brew shellenv -|psub)
+    # fish_add_path -U $HOMEBREW_PREFIX/bin
     starship init fish | source
     zoxide init fish | source
     navi widget fish | source
@@ -49,4 +28,15 @@ end
 # status is-login; and pyenv init --path | source
 # status is-interactive; and pyenv init - | source
 
-test -e {$HOME}/.iterm2_shell_integration.fish; and source {$HOME}/.iterm2_shell_integration.fish
+test -e $HOME/.iterm2_shell_integration.fish; and source $HOME/.iterm2_shell_integration.fish
+
+# https://www.reddit.com/r/fishshell/comments/govh0k/recommended_way_of_setting_environment_vars_in/
+set -x LANG en_US.UTF-8
+set -x XDG_CONFIG_HOME $HOME/.config
+set -x JAVA_HOME $HOME/.asdf/installs/java/microsoft-17.0.6
+
+# set fish_user_paths
+fish_add_path $HOME/.anyenv/bin
+fish_add_path $XDG_CONFIG_HOME/emacs/bin
+
+abbr -a emacs emacsclient -nw
