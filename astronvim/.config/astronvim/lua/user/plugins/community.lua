c81a4d4c8bfcbffb3b6695a879b54e12ceecdb58
Ken Chu <chuwingyu@gmail.com>
2023-07-17 00:22:39 +0800

    [neovim] feat(neogit): add NeogitLogView to disable smartcolumn list.

--- astronvim/.config/astronvim/lua/user/plugins/community.lua
+++ astronvim/.config/astronvim/lua/user/plugins/community.lua
@@ -14,7 +14,7 @@ return {
 		"m4xshen/smartcolumn.nvim",
 		opts = {
 			colorcolumn = "120",
-			disabled_filetypes = { "NvimTree", "lazy", "mason", "help", "text", "markdown" },
+			disabled_filetypes = { "NvimTree", "lazy", "mason", "help", "text", "markdown", "NeogitLogView" },
 			scope = "window",
 		},
 	},
