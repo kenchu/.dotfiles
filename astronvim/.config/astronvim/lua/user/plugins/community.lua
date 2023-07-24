9da46bcc99d5d39f4657db782a78c815062addb3
Ken Chu <chuwingyu@gmail.com>
2023-07-25 02:11:28 +0800

    [neovim] feat(astronvim-community): add several editing-support plugins

--- astronvim/.config/astronvim/lua/user/plugins/community.lua
+++ astronvim/.config/astronvim/lua/user/plugins/community.lua
@@ -3,6 +3,7 @@ return {
 	"AstroNvim/astrocommunity",
 	-- example of imporing a plugin, comment out to use it or add your own
 	-- available plugins can be found at https://github.com/AstroNvim/astrocommunity
+
 	{ import = "astrocommunity.colorscheme.catppuccin" },
 	{ import = "astrocommunity.colorscheme.dracula-nvim" },
 	{ import = "astrocommunity.colorscheme.tokyonight-nvim" },
@@ -19,55 +20,78 @@ return {
 		},
 	},
 	{ import = "astrocommunity.bars-and-lines.vim-illuminate" },
+
 	{ import = "astrocommunity.code-runner.compiler-nvim" },
 	{ import = "astrocommunity.code-runner.sniprun" },
+
 	{ import = "astrocommunity.color.ccc-nvim" },
 	{ import = "astrocommunity.color.headlines-nvim" },
-	{ import = "astrocommunity.color.modes-nvim", enabled = false },
+	{ import = "astrocommunity.color.modes-nvim", enabled = true },
 	{ import = "astrocommunity.color.twilight-nvim" },
+
 	{ import = "astrocommunity.completion.cmp-cmdline" },
 	{ import = "astrocommunity.completion.codeium-vim" },
+
 	{ import = "astrocommunity.debugging.nvim-bqf" },
 	{ import = "astrocommunity.debugging.telescope-dap-nvim" },
+
 	{ import = "astrocommunity.diagnostics.trouble-nvim" },
+
+	{ import = "astrocommunity.editing-support.hypersonic-nvim" },
+	{ import = "astrocommunity.editing-support.multicursors-nvim", enabled = false },
 	{ import = "astrocommunity.editing-support.nvim-regexplainer" },
 	{ import = "astrocommunity.editing-support.suda-vim" },
+	{ import = "astrocommunity.editing-support.telescope-undo-nvim" },
+	{ import = "astrocommunity.editing-support.refactoring-nvim" },
+	{ import = "astrocommunity.fuzzy-finder.telescope-zoxide" },
+
 	{ import = "astrocommunity.git.diffview-nvim" },
 	{ import = "astrocommunity.git.git-blame-nvim" },
 	{ import = "astrocommunity.git.neogit" },
+
 	{ import = "astrocommunity.indent.indent-blankline-nvim" },
 	{ import = "astrocommunity.indent.indent-tools-nvim" },
 	{ import = "astrocommunity.indent.mini-indentscope" },
+
 	{ import = "astrocommunity.motion.flash-nvim" },
+	{ import = "astrocommunity.motion.marks-nvim" },
 	{ import = "astrocommunity.motion.mini-ai" },
+	{ import = "astrocommunity.motion.mini-basics" },
 	{ import = "astrocommunity.motion.mini-move" },
 	{ import = "astrocommunity.motion.mini-surround" },
 	-- { import = "astrocommunity.motion.nvim-surround" },
 	{ import = "astrocommunity.motion.nvim-spider" },
+
 	{ import = "astrocommunity.project.nvim-spectre" },
 	-- { import = "astrocommunity.project.project-nvim" },
 	{ import = "astrocommunity.project.projectmgr-nvim" },
+
 	{ import = "astrocommunity.scrolling.satellite-nvim" },
+
 	{ import = "astrocommunity.split-and-window.edgy-nvim" },
+
 	{ import = "astrocommunity.syntax.vim-easy-align" },
+
 	{ import = "astrocommunity.utility.neodim" },
 	{ import = "astrocommunity.utility.nvim-toggler" },
 	{ import = "astrocommunity.utility.noice-nvim" },
 	{ import = "astrocommunity.utility.transparent-nvim" },
 
+	{ import = "astrocommunity.pack.ansible" },
 	{ import = "astrocommunity.pack.docker" },
-	{ import = "astrocommunity.pack.proto" },
+	{ import = "astrocommunity.pack.go" },
+	{ import = "astrocommunity.pack.html-css" },
+	{ import = "astrocommunity.pack.java" },
 	{ import = "astrocommunity.pack.json" },
+	{ import = "astrocommunity.pack.kotlin" },
+	{ import = "astrocommunity.pack.lua" },
+	{ import = "astrocommunity.pack.proto" },
+	{ import = "astrocommunity.pack.python" },
+	{ import = "astrocommunity.pack.rust" },
+	{ import = "astrocommunity.pack.svelte" },
 	{ import = "astrocommunity.pack.toml" },
 	{ import = "astrocommunity.pack.yaml" },
-	{ import = "astrocommunity.pack.lua" },
-	{ import = "astrocommunity.pack.rust" },
-	{ import = "astrocommunity.pack.go" },
-	{ import = "astrocommunity.pack.java" },
-	{ import = "astrocommunity.pack.kotlin" },
-	{ import = "astrocommunity.pack.python" },
-	{ import = "astrocommunity.pack.svelte" },
-	{ import = "astrocommunity.pack.html-css" },
+
 	{ import = "astrocommunity.programming-language-support.nvim-jqx" },
-	{ import = "astrocommunity.programming-language-support.rest-nvim" },
+	{ import = "astrocommunity.programming-language-support.rest-nvim", enabled = false },
 }
