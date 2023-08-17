c79dc2d638a837dccb32080984dbafe3b89d1edc
Ken Chu <chuwingyu@gmail.com>
2023-08-18 01:01:10 +0800

    [neovim] refactor(telescope): integrate different telescope extensions into one telescope plugin spec

--- astronvim/.config/astronvim/lua/user/plugins/telescope.lua
+++ astronvim/.config/astronvim/lua/user/plugins/telescope.lua
@@ -1,15 +1,33 @@
 return {
 
 	{
-		"telescope.nvim",
+		"nvim-telescope/telescope.nvim",
 		dependencies = {
 			"natecraddock/telescope-zf-native.nvim",
+			{
+				"nvim-telescope/telescope-frecency.nvim",
+				dependencies = { "kkharji/sqlite.lua" },
+			},
 			"nvim-telescope/telescope-file-browser.nvim",
-			"nvim-telescope/telescope-frecency.nvim",
 			"nvim-telescope/telescope-media-files.nvim",
 			"nvim-telescope/telescope-project.nvim",
+			"tiagovla/scope.nvim",
 			"olacin/telescope-cc.nvim",
 		},
+		config = function(plugin, opts)
+			-- run the core AstroNvim configuration function with the options table
+			require("plugins.configs.telescope")(plugin, opts)
+
+			-- require telescope and load extensions as necessary
+			local telescope = require("telescope")
+			telescope.load_extension("media_files")
+			telescope.load_extension("conventional_commits")
+			telescope.load_extension("file_browser")
+			telescope.load_extension("frecency")
+			telescope.load_extension("media_files")
+			telescope.load_extension("project")
+			telescope.load_extension("scope")
+		end,
 		keys = {
 			{ "<leader><space>", "<cmd>Telescope frecency workspace=CWD<cr>", desc = "Find frecency" },
 			{ "<leader>fe", "<cmd>Telescope file_browser<cr>", desc = "File browser" },
@@ -42,7 +60,7 @@ return {
 					base_dirs = {
 						{ path = "~/repo", max_depth = 1 },
 						{ path = "~/repo/work", max_depth = 2 },
-						{ path = "~/repo/gis", max_depth = 3 },
+						{ path = "~/ghq", max_depth = 3 },
 					},
 					hidden_files = true, -- default: false
 					theme = "dropdown",
@@ -57,62 +75,5 @@ return {
 				},
 			},
 		},
-	},
-
-	{
-		"nvim-telescope/telescope-project.nvim",
-		dependencies = { "nvim-telescope/telescope-file-browser.nvim" },
-		config = function()
-			require("telescope").load_extension("project")
-		end,
-	},
-
-	-- {
-	-- 	"cljoly/telescope-repo.nvim",
-	-- 	dependencies = { "nvim-telescope/telescope.nvim", "nvim-lua/plenary.nvim" },
-	-- 	config = function()
-	-- 		require("telescope").load_extension("repo")
-	-- 	end,
-	-- 	keys = {
-	-- 		{ "<leader>fp", "<cmd>Telescope repo list search_dirs=~/repo<cr>", desc = "Find projects" },
-	-- 	},
-	-- },
-
-	{
-		"tiagovla/scope.nvim",
-		config = function()
-			require("telescope").load_extension("scope")
-		end,
-	},
-
-	{
-		"nvim-telescope/telescope-file-browser.nvim",
-		dependencies = { "nvim-telescope/telescope.nvim", "nvim-lua/plenary.nvim" },
-		config = function()
-			require("telescope").load_extension("file_browser")
-		end,
-	},
-
-	{
-		"nvim-telescope/telescope-media-files.nvim",
-		config = function()
-			require("telescope").load_extension("media_files")
-		end,
-	},
-
-	{
-		"nvim-telescope/telescope-frecency.nvim",
-		dependencies = { "kkharji/sqlite.lua" },
-		config = function()
-			require("telescope").load_extension("frecency")
-		end,
-	},
-
-	{
-		"olacin/telescope-cc.nvim",
-		dependencies = { "nvim-telescope/telescope.nvim", "nvim-lua/plenary.nvim" },
-		config = function()
-			require("telescope").load_extension("conventional_commits")
-		end,
 	},
 }
