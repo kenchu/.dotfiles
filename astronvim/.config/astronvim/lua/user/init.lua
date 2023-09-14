f0d98519fde0b5fc3a10097ccdc1900c9f9dfe00
Ken Chu <chuwingyu@gmail.com>
2023-09-15 02:57:01 +0800

    [neovim] fix(lazy.nvim): limit the concurrency = 64 to prevent from update failure.

--- astronvim/.config/astronvim/lua/user/init.lua
+++ astronvim/.config/astronvim/lua/user/init.lua
@@ -1,1 +1,11 @@
-return {}
+-- https://astronvim.com/Configuration/config_options
+return {
+	-- https://github.com/folke/lazy.nvim
+	lazy = {
+		concurrency = 64,
+		change_detection = {
+			enabled = true,
+			notify = true,
+		},
+	},
+}
