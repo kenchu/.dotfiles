c733ce4878eb786f9054a0c5de549bfc8e5f9c5c
Ken Chu <chuwingyu@gmail.com>
2023-09-15 03:04:06 +0800

    [neovim] fix(codeium): change key maps to avoid conflict mini-move lines.

--- astronvim/.config/astronvim/lua/user/plugins/codeium.lua
+++ astronvim/.config/astronvim/lua/user/plugins/codeium.lua
@@ -2,23 +2,23 @@ return {
 	"Exafunction/codeium.vim",
 	lazy = false,
 	cmd = "Codeium",
-	init = function()
-		-- vim.g.codeium_enabled = 1
-		-- vim.g.codeium_disable_bindings = 1
-		vim.g.codeium_idle_delay = 500
-		-- match function
-	end,
+	-- init = function()
+	-- 	vim.g.codeium_enabled = 1
+	-- 	-- vim.g.codeium_disable_bindings = 1
+	-- 	-- vim.g.codeium_idle_delay = 1000
+	-- 	-- match function
+	-- end,
 	config = function()
-		vim.keymap.set("i", "<A-CR>", function()
+		vim.keymap.set("i", "<C-CR>", function()
 			return vim.fn["codeium#Accept"]()
 		end, { expr = true })
-		vim.keymap.set("i", "<A-j>", function()
+		vim.keymap.set("i", "<C-n>", function()
 			return vim.fn["codeium#CycleCompletions"](1)
 		end, { expr = true })
-		vim.keymap.set("i", "<A-k>", function()
+		vim.keymap.set("i", "<C-p>", function()
 			return vim.fn["codeium#CycleCompletions"](-1)
 		end, { expr = true })
-		vim.keymap.set("i", "<A-BS>", function()
+		vim.keymap.set("i", "<C-BS>", function()
 			return vim.fn["codeium#Clear"]()
 		end, { expr = true })
 		vim.api.nvim_set_hl(0, "CodeiumSuggestion", { link = "Comment" })
