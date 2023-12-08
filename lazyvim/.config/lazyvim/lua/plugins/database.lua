3ea41ce518cbf60cdfc61e89ab9b091d614b60f8
Ken Chu <chuwingyu@gmail.com>
2023-12-08 20:20:50 +0800

    [neovim] feat(database): add vim-dadbod plugin

---
+++ lazyvim/.config/lazyvim/lua/plugins/database.lua
@@ -0,0 +1,26 @@
+return {
+  {
+    "tpope/vim-dadbod",
+    dependencies = {
+      "kristijanhusak/vim-dadbod-ui",
+      { "kristijanhusak/vim-dadbod-completion", ft = { "sql", "mysql", "plsql" }, lazy = true },
+    },
+    -- init = function()
+    --   -- Your DBUI configuration
+    --   vim.g.db_ui_use_nerd_fonts = 1
+    -- end,
+    cmd = {
+      "DBUI",
+      "DBUIToggle",
+      "DBUIAddConnection",
+      "DBUIFindBuffer",
+    },
+    keys = {
+      { "<leader>D", desc = "+database" },
+      { "<leader>Du", "<cmd>DBUIToggle<cr>", desc = "DB UI Toggle" },
+      { "<leader>Df", "<cmd>DBUIFindBuffer<cr>", desc = "DB UI Find Buffer" },
+      { "<leader>Dr", "<cmd>DBUIRenameBuffer<cr>", desc = "DB UI Rename Buffer" },
+      { "<leader>Dq", "<cmd>DBUILastQueryInfo<cr>", desc = "DB UI Last Query Info" },
+    },
+  },
+}
