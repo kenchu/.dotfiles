4a2f6addcdc33b39093b895551dd63ef250b36cc
Ken Chu <chuwingyu@gmail.com>
2023-12-18 15:44:13 +0800

    [neovim] feat(database): change database UI keymap

--- lazyvim/.config/lazyvim/lua/plugins/database.lua
+++ lazyvim/.config/lazyvim/lua/plugins/database.lua
@@ -17,7 +17,7 @@ return {
     },
     keys = {
       { "<leader>D", desc = "+database" },
-      { "<leader>Du", "<cmd>DBUIToggle<cr>", desc = "DB UI Toggle" },
+      { "<leader>ud", "<cmd>DBUIToggle<cr>", desc = "DB UI Toggle" },
       { "<leader>Df", "<cmd>DBUIFindBuffer<cr>", desc = "DB UI Find Buffer" },
       { "<leader>Dr", "<cmd>DBUIRenameBuffer<cr>", desc = "DB UI Rename Buffer" },
       { "<leader>Dq", "<cmd>DBUILastQueryInfo<cr>", desc = "DB UI Last Query Info" },
