db59b4ef4dd9e3f20ffa4fb846e2b1f6696f4a6c
Ken Chu <chuwingyu@gmail.com>
2023-12-26 02:16:37 +0800

    [neovim] refactor: keymaps and options

--- lazyvim/.config/lazyvim/lua/config/keymaps.lua
+++ lazyvim/.config/lazyvim/lua/config/keymaps.lua
@@ -4,7 +4,7 @@
 
 vim.keymap.set("n", "<leader>h", "<cmd>Dashboard<cr>", { desc = "Dashboard" })
 
--- basic keymap
+-- window keymap
 vim.keymap.set("n", "<leader>w", "<cmd>w<cr><esc>", { desc = "Write File" })
 vim.keymap.set("n", "<leader>ws", "<cmd>sp<cr><esc>", { desc = "Window Split" })
 vim.keymap.set("n", "<leader>wv", "<cmd>vsp<cr><esc>", { desc = "Window Vertical Split" })
@@ -61,10 +61,6 @@ neovide.setup = function()
   if vim.g.neovide then
     -- Put anything you want to happen only in Neovide here
 
-    vim.g.neovide_profiler = false
-
-    vim.g.neovide_theme = "auto"
-    vim.g.neovide_confirm_quit = true
     vim.g.neovide_input_macos_alt_is_meta = true
     vim.g.neovide_input_use_logo = true -- true on macOS
 
@@ -102,59 +98,6 @@ neovide.setup = function()
     vim.keymap.set("n", "<D-->", function()
       change_scale_factor(1 / 1.25)
     end)
-
-    -- Helper function for transparency formatting
-    local alpha = function()
-      return string.format("%x", math.floor(255 * (vim.g.transparency or 0.8)))
-    end
-    -- g:neovide_transparency should be 0 if you want to unify transparency of content and title bar.
-    -- vim.g.neovide_transparency = 0.0
-    -- vim.g.transparency = 0.9
-    -- vim.g.neovide_background_color = "#000000" .. alpha()
-    vim.g.neovide_floating_blur_amount_x = 2.0
-    vim.g.neovide_floating_blur_amount_y = 2.0
-    -- vim.g.neovide_refresh_rate = 60
-    -- vim.g.neovide_refresh_rate_idle = 5
-    -- vim.g.neovide_fullscreen = false
-    -- vim.g.neovide_remember_window_size = true
-
-    -- vim.g.neovide_hide_mouse_when_typing = true
-    -- vim.g.neovide_touch_deadzone = 6.0
-    -- vim.g.neovide_touch_drag_timeout = 0.7
-    vim.g.neovide_scroll_animation_length = 0.3
-    -- vim.g.neovide_cursor_animation_length = 0.13
-    -- vim.g.neovide_cursor_trail_size = 0.8
-    vim.g.neovide_cursor_antialiasing = true
-    vim.g.neovide_cursor_animate_in_insert_mode = true
-    vim.g.neovide_cursor_animate_command_line = true
-    -- vim.g.neovide_cursor_vfx_mode = "sonicboom"
-    vim.g.neovide_cursor_vfx_mode = "ripple"
-    -- vim.g.neovide_cursor_vfx_opacity = 200.0
-    -- vim.g.neovide_cursor_vfx_particle_lifetime = 1.2
-    -- vim.g.neovide_cursor_vfx_particle_density = 16.0
-    -- vim.g.neovide_cursor_vfx_particle_speed = 4.0
-
-    local function set_ime(args)
-      if args.event:match("Enter$") then
-        vim.g.neovide_input_ime = true
-      else
-        vim.g.neovide_input_ime = false
-      end
-    end
-
-    local ime_input = vim.api.nvim_create_augroup("ime_input", { clear = true })
-
-    vim.api.nvim_create_autocmd({ "InsertEnter", "InsertLeave" }, {
-      group = ime_input,
-      pattern = "*",
-      callback = set_ime,
-    })
-
-    vim.api.nvim_create_autocmd({ "CmdlineEnter", "CmdlineLeave" }, {
-      group = ime_input,
-      pattern = "[/\\?]",
-      callback = set_ime,
-    })
   end
 end
 
--- lazyvim/.config/lazyvim/lua/config/options.lua
+++ lazyvim/.config/lazyvim/lua/config/options.lua
@@ -6,3 +6,77 @@ vim.opt.guifont = "JetBrainsMono Nerd Font:12"
 
 -- TODO: not sure if it works
 vim.opt.pumblend = vim.g.winblend
+
+if vim.g.neovide then
+  -- Put anything you want to happen only in Neovide here
+
+  vim.g.neovide_profiler = false
+  vim.g.neovide_theme = "dark"
+  -- vim.g.neovide_confirm_quit = true
+
+  -- Helper function for transparency formatting
+  -- local alpha = function()
+  --   return string.format("%x", math.floor(255 * (vim.g.transparency or 0.8)))
+  -- end
+  -- g:neovide_transparency should be 0 if you want to unify transparency of content and title bar.
+  vim.g.neovide_transparency = 0.9
+  -- vim.g.transparency = 0.9
+  -- vim.g.neovide_background_color = "#000000" .. alpha()
+  -- vim.g.neovide_floating_blur_amount_x = 2.0
+  -- vim.g.neovide_floating_blur_amount_y = 2.0
+  -- vim.g.neovide_floating_shadow = true
+  -- vim.g.neovide_floating_z_height = 10
+  -- vim.g.neovide_light_angle_degrees = 45
+  -- vim.g.neovide_light_radius = 5
+
+  -- vim.g.neovide_refresh_rate = 60
+  -- vim.g.neovide_refresh_rate_idle = 5
+  -- vim.g.neovide_fullscreen = false
+  -- vim.g.neovide_remember_window_size = true
+
+  -- vim.g.neovide_touch_deadzone = 6.0
+  -- vim.g.neovide_touch_drag_timeout = 0.7
+  -- vim.g.neovide_scroll_animation_length = 0.3
+  -- vim.g.neovide_scroll_animation_far_lines = 1
+  vim.g.neovide_hide_mouse_when_typing = true
+  -- vim.g.neovide_underline_stroke_scale = 1.0
+  -- vim.g.neovide_unlink_border_highlights = true
+
+  -- vim.g.neovide_cursor_animation_length = 0.13
+  -- vim.g.neovide_cursor_trail_size = 0.8
+  -- vim.g.neovide_cursor_antialiasing = true
+  -- vim.g.neovide_cursor_animate_in_insert_mode = true
+  -- vim.g.neovide_cursor_animate_command_line = true
+  -- vim.g.neovide_cursor_unfocused_outline_width = 0.125
+  -- vim.g.neovide_cursor_vfx_mode = "sonicboom"
+  -- vim.g.neovide_cursor_vfx_mode = "ripple"
+  vim.g.neovide_cursor_vfx_mode = "railgun"
+  -- vim.g.neovide_cursor_vfx_opacity = 200.0
+  -- vim.g.neovide_cursor_vfx_particle_lifetime = 1.2
+  vim.g.neovide_cursor_vfx_particle_density = 16.0
+  -- vim.g.neovide_cursor_vfx_particle_speed = 10.0
+  -- vim.g.neovide_cursor_vfx_particle_phase = 1.5
+  -- vim.g.neovide_cursor_vfx_particle_curl = 1.0
+
+  local function set_ime(args)
+    if args.event:match("Enter$") then
+      vim.g.neovide_input_ime = true
+    else
+      vim.g.neovide_input_ime = false
+    end
+  end
+
+  local ime_input = vim.api.nvim_create_augroup("ime_input", { clear = true })
+
+  vim.api.nvim_create_autocmd({ "InsertEnter", "InsertLeave" }, {
+    group = ime_input,
+    pattern = "*",
+    callback = set_ime,
+  })
+
+  vim.api.nvim_create_autocmd({ "CmdlineEnter", "CmdlineLeave" }, {
+    group = ime_input,
+    pattern = "[/\\?]",
+    callback = set_ime,
+  })
+end
