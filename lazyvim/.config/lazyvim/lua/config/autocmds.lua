33800551eb388ca4331c3df76fb72b2c29ad4966
Ken Chu <chuwingyu@gmail.com>
2023-12-26 18:29:41 +0800

    [neovim] refactor(autocmds): move code form options to autocmds

--- lazyvim/.config/lazyvim/lua/config/autocmds.lua
+++ lazyvim/.config/lazyvim/lua/config/autocmds.lua
@@ -12,3 +12,27 @@ vim.api.nvim_create_autocmd("VimEnter", {
     end
   end,
 })
+
+if vim.g.neovide then
+  local function set_ime(args)
+    if args.event:match("Enter$") then
+      vim.g.neovide_input_ime = true
+    else
+      vim.g.neovide_input_ime = false
+    end
+  end
+
+  local ime_input = vim.api.nvim_create_augroup("ime_input", { clear = true })
+
+  vim.api.nvim_create_autocmd({ "InsertEnter", "InsertLeave" }, {
+    group = ime_input,
+    pattern = "*",
+    callback = set_ime,
+  })
+
+  vim.api.nvim_create_autocmd({ "CmdlineEnter", "CmdlineLeave" }, {
+    group = ime_input,
+    pattern = "[/\\?]",
+    callback = set_ime,
+  })
+end
