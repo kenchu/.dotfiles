b843db71939d47b1bc0bc6dcbf6e77fd579f7b03
Ken Chu <chuwingyu@gmail.com>
2023-12-26 18:34:27 +0800

    [neovim] feat(gist): add gist.nvim

--- lazyvim/.config/lazyvim/lua/plugins/git.lua
+++ lazyvim/.config/lazyvim/lua/plugins/git.lua
@@ -14,6 +14,30 @@ return {
   },
 
   {
+    "Rawnly/gist.nvim",
+    dependencies = {
+      {
+        "samjwill/nvim-unception",
+        lazy = false,
+        init = function()
+          vim.g.unception_block_while_host_edits = true
+        end,
+      },
+    },
+    cmd = { "GistCreate", "GistCreateFromFile", "GistsList" },
+    opts = {
+      list = {
+        -- If there are multiple files in a gist you can scroll them,
+        -- with vim-like bindings j/k next previous
+        mappings = {
+          next_file = "<C-j>",
+          prev_file = "<C-k>",
+        },
+      },
+    },
+  },
+
+  {
     "NeogitOrg/neogit",
     dependencies = {
       "nvim-lua/plenary.nvim", -- required
