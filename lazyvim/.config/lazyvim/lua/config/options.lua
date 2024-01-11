7298dfefcfff972619d36615225352312d348ef9
Ken Chu <chuwingyu@gmail.com>
2024-01-12 02:26:42 +0800

    [neovim] style: less transparency

--- lazyvim/.config/lazyvim/lua/config/options.lua
+++ lazyvim/.config/lazyvim/lua/config/options.lua
@@ -19,7 +19,7 @@ if vim.g.neovide then
   --   return string.format("%x", math.floor(255 * (vim.g.transparency or 0.8)))
   -- end
   -- g:neovide_transparency should be 0 if you want to unify transparency of content and title bar.
-  vim.g.neovide_transparency = 0.9
+  vim.g.neovide_transparency = 0.95
   -- vim.g.transparency = 0.9
   -- vim.g.neovide_background_color = "#000000" .. alpha()
   -- vim.g.neovide_floating_blur_amount_x = 2.0
