55fc475687ec7a0d9183469432631c41051fc764
Ken Chu <chuwingyu@gmail.com>
2024-02-26 02:04:33 +0800

    [neovim] style(project): group dashboard.nvim into project.lua

--- lazyvim/.config/lazyvim/lua/plugins/project.lua
+++ lazyvim/.config/lazyvim/lua/plugins/project.lua
@@ -8,4 +8,22 @@ return {
       scope_chdir = "tab",
     },
   },
+
+  {
+    "nvimdev/dashboard-nvim",
+    optional = true,
+    opts = function(_, opts)
+      local projects = {
+        action = "Telescope project",
+        desc = " Projects",
+        icon = " ",
+        key = "p",
+      }
+
+      projects.desc = projects.desc .. string.rep(" ", 43 - #projects.desc)
+      projects.key_format = "  %s"
+
+      table.insert(opts.config.center, 3, projects)
+    end,
+  },
 }
