857ae6641bffb8bcacbe7918e381bfc21128c0cc
Ken Chu <chuwingyu@gmail.com>
2024-06-04 16:29:12 +0800

    feat(neovim): set 'yarn' as nodejs package manager for compiler.nvim

--- lazyvim/.config/lazyvim/lua/config/options.lua
+++ lazyvim/.config/lazyvim/lua/config/options.lua
@@ -2,6 +2,8 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 
+vim.g.NODEJS_PACKAGE_MANAGER = "yarn"
+
 vim.o.guifont = "JetBrainsMono Nerd Font:h13"
 -- vim.opt.linespace = 4
 
