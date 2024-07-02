3efa493d84cbc0b97dfec2865f27b062189b2856
Ken Chu <chuwingyu@gmail.com>
2024-07-03 00:20:23 +0800

    feat(fish): add function `nv` for neovim config manager

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -44,4 +44,19 @@ fish_add_path $XDG_CONFIG_HOME/emacs/bin
 # abbr -a emacs emacsclient -nw
 abbr -a ldk lazydocker
 abbr -a lgit lazygit
-abbr -a nv neovide
+# abbr -a nv neovide
+
+function nv
+    # Assumes all configs exist in directories named ~/.config/nvim-*
+    set -l config (fd --max-depth 1 --glob 'nvim-*' ~/.config | fzf --prompt="Neovim Configs > " --height=~50% --layout=reverse --border --exit-0)
+
+    # If I exit fzf without selecting a config, don't open Neovim
+    if test -z "$config"
+        echo "No config selected"
+        return
+    end
+
+    # Open Neovim with the selected config
+    set -l NVIM_APPNAME (basename $config)
+    NVIM_APPNAME=$NVIM_APPNAME nvim $argv
+end
