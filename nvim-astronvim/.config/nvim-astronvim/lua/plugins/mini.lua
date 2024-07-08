b0e2c1f9e241c19a46debd361564f9219d617165
Ken Chu <chuwingyu@gmail.com>
2024-07-08 17:12:54 +0800

    feat(astronvim): add keymaps for mini-files

--- nvim-astronvim/.config/nvim-astronvim/lua/plugins/mini.lua
+++ nvim-astronvim/.config/nvim-astronvim/lua/plugins/mini.lua
@@ -29,7 +29,7 @@ return {
     end,
   },
 
-  { "folke/which-key.nvim", enabled = true },
+  -- { "folke/which-key.nvim", enabled = true },
   {
     "echasnovski/mini.clue",
     opts = {
@@ -50,7 +50,15 @@ return {
             n = {
               ["<Leader>fm"] = {
                 function()
-                  if not require("mini.files").close() then require("mini.files").open() end
+                  if not require("mini.files").close() then
+                    require("mini.files").open(vim.api.nvim_buf_get_name(0), true)
+                  end
+                end,
+                desc = "Find mini-files",
+              },
+              ["<Leader>fM"] = {
+                function()
+                  if not require("mini.files").close() then require("mini.files").open(vim.uv.cwd(), true) end
                 end,
                 desc = "Find mini-files",
               },
@@ -59,7 +67,71 @@ return {
         },
       },
     },
-    opts = {},
+    config = function(_, opts)
+      require("mini.files").setup(opts)
+
+      local show_dotfiles = true
+      local filter_show = function(fs_entry) return true end
+      local filter_hide = function(fs_entry) return not vim.startswith(fs_entry.name, ".") end
+
+      local toggle_dotfiles = function()
+        show_dotfiles = not show_dotfiles
+        local new_filter = show_dotfiles and filter_show or filter_hide
+        require("mini.files").refresh { content = { filter = new_filter } }
+      end
+
+      local map_split = function(buf_id, lhs, direction, close_on_file)
+        local rhs = function()
+          local new_target_window
+          local cur_target_window = require("mini.files").get_target_window()
+          if cur_target_window ~= nil then
+            vim.api.nvim_win_call(cur_target_window, function()
+              vim.cmd("belowright " .. direction .. " split")
+              new_target_window = vim.api.nvim_get_current_win()
+            end)
+
+            require("mini.files").set_target_window(new_target_window)
+            require("mini.files").go_in { close_on_file = close_on_file }
+          end
+        end
+
+        local desc = "Open in " .. direction .. " split"
+        if close_on_file then desc = desc .. " and close" end
+        vim.keymap.set("n", lhs, rhs, { buffer = buf_id, desc = desc })
+      end
+
+      local files_set_cwd = function()
+        local cur_entry_path = require("mini.files").get_fs_entry().path
+        local cur_directory = vim.fs.dirname(cur_entry_path)
+        if cur_directory ~= nil then vim.fn.chdir(cur_directory) end
+      end
+
+      vim.api.nvim_create_autocmd("User", {
+        pattern = "MiniFilesBufferCreate",
+        callback = function(args)
+          local buf_id = args.data.buf_id
+
+          vim.keymap.set(
+            "n",
+            opts.mappings and opts.mappings.toggle_hidden or "g.",
+            toggle_dotfiles,
+            { buffer = buf_id, desc = "Toggle hidden files" }
+          )
+
+          vim.keymap.set(
+            "n",
+            opts.mappings and opts.mappings.change_cwd or "gc",
+            files_set_cwd,
+            { buffer = args.data.buf_id, desc = "Set cwd" }
+          )
+
+          map_split(buf_id, opts.mappings and opts.mappings.go_in_horizontal or "<C-w>s", "horizontal", false)
+          map_split(buf_id, opts.mappings and opts.mappings.go_in_vertical or "<C-w>v", "vertical", false)
+          map_split(buf_id, opts.mappings and opts.mappings.go_in_horizontal_plus or "<C-w>S", "horizontal", true)
+          map_split(buf_id, opts.mappings and opts.mappings.go_in_vertical_plus or "<C-w>V", "vertical", true)
+        end,
+      })
+    end,
   },
 
   {
