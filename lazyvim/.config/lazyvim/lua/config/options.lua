bd8947acb3182677401c8f043037392816664252
Ken Chu <chuwingyu@gmail.com>
2024-07-09 01:03:24 +0800

    feat(lazyvim): use bun as default package manager

--- lazyvim/.config/lazyvim/lua/config/options.lua
+++ lazyvim/.config/lazyvim/lua/config/options.lua
@@ -2,7 +2,7 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 
-vim.g.NODEJS_PACKAGE_MANAGER = "yarn"
+vim.g.NODEJS_PACKAGE_MANAGER = "bun"
 
 vim.o.guifont = "JetBrainsMono Nerd Font:h13"
 -- vim.opt.linespace = 4
