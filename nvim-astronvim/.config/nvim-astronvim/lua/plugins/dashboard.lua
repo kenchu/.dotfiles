c5fc394d00508439cbd4751a569248de4d10ecfa
Ken Chu <chuwingyu@gmail.com>
2024-07-11 15:22:20 +0800

    feat(astronvim): add dashboard button for telescope zoxide list

--- nvim-astronvim/.config/nvim-astronvim/lua/plugins/dashboard.lua
+++ nvim-astronvim/.config/nvim-astronvim/lua/plugins/dashboard.lua
@@ -6,12 +6,13 @@ return {
       opts.button("f", "  Find File", "<cmd>Telescope find_files<cr>"),
       opts.button("r", "  Recently Files", "<cmd>Telescope oldfiles<cr>"),
       opts.button("g", "  Find Text", "<cmd>Telescope live_grep<cr>"),
-      opts.button("p", "  Project", "<cmd>Telescope project<cr>"),
+      opts.button("p", "  Project", "<cmd>Telescope project<cr>"),
+      opts.button("z", "  Zoxide", "<cmd>Telescope zoxide list<cr>"),
       opts.button("s", "  Load Session", "<cmd>lua require('resession').load()<cr>"),
       opts.button("l", "  Lazy", "<cmd>Lazy<cr>"),
       opts.button(
         "c",
-        "  Config",
+        "  Configuration",
         function() require("telescope.builtin").find_files { cwd = vim.fn.stdpath "config" } end
       ),
       opts.button("q", "  Quit Neovim", ":qa<CR>"),
