b2d6aebed60d556005dbb61e9b951d9177f6bd63
Ken Chu <chuwingyu@gmail.com>
2024-07-17 20:20:32 +0800

    feat(astronvim): change cmp formatting as lazyvim one

--- nvim-astronvim/.config/nvim-astronvim/lua/plugins/cmp.lua
+++ nvim-astronvim/.config/nvim-astronvim/lua/plugins/cmp.lua
@@ -13,43 +13,43 @@ return {
         ["<C-CR>"] = cmp.mapping(function() cmp.abort() end, { "i", "s" }),
         ["<S-CR>"] = cmp.mapping(function() cmp.confirm { behavior = cmp.ConfirmBehavior.Replace } end, { "i", "s" }),
       },
-      -- formatting = {
-      --   fields = { "abbr", "kind", "menu" },
-      --   format = function(entry, vim_item)
-      --     if vim.tbl_contains({ "path" }, entry.source.name) then
-      --       local icon, hl_group = require("nvim-web-devicons").get_icon(entry:get_completion_item().label)
-      --       if icon then
-      --         vim_item.kind = icon
-      --         vim_item.kind_hl_group = hl_group
-      --         return vim_item
-      --       end
-      --     end
-      --
-      --     -- lspkind cmp format
-      --     vim_item = require("lspkind").cmp_format {
-      --       preset = "codicons",
-      --       mode = "symbol_text",
-      --       maxwidth = function() return math.floor(0.45 * vim.o.columns) end,
-      --       ellipsis_char = "…",
-      --       show_labelDetails = true,
-      --     }(entry, vim_item)
-      --
-      --     -- lazyvim cmp window format
-      --     -- ref: https://www.lazyvim.org/plugins/coding
-      --     local widths = {
-      --       abbr = vim.g.cmp_widths and vim.g.cmp_widths.abbr or 40,
-      --       menu = vim.g.cmp_widths and vim.g.cmp_widths.menu or 30,
-      --     }
-      --
-      --     for key, width in pairs(widths) do
-      --       if vim_item[key] and vim.fn.strdisplaywidth(vim_item[key]) > width then
-      --         vim_item[key] = vim.fn.strcharpart(vim_item[key], 0, width - 1) .. "…"
-      --       end
-      --     end
-      --
-      --     return vim_item
-      --   end,
-      -- },
+      formatting = {
+        fields = { "abbr", "kind", "menu" },
+        format = function(entry, vim_item)
+          if vim.tbl_contains({ "path" }, entry.source.name) then
+            local icon, hl_group = require("nvim-web-devicons").get_icon(entry:get_completion_item().label)
+            if icon then
+              vim_item.kind = icon
+              vim_item.kind_hl_group = hl_group
+              return vim_item
+            end
+          end
+
+          -- lspkind cmp format
+          vim_item = require("lspkind").cmp_format {
+            -- preset = "codicons",
+            mode = "symbol_text",
+            maxwidth = function() return math.floor(0.45 * vim.o.columns) end,
+            ellipsis_char = "…",
+            show_labelDetails = true,
+          }(entry, vim_item)
+
+          -- lazyvim cmp window format
+          -- ref: https://www.lazyvim.org/plugins/coding
+          local widths = {
+            abbr = vim.g.cmp_widths and vim.g.cmp_widths.abbr or 40,
+            menu = vim.g.cmp_widths and vim.g.cmp_widths.menu or 30,
+          }
+
+          for key, width in pairs(widths) do
+            if vim_item[key] and vim.fn.strdisplaywidth(vim_item[key]) > width then
+              vim_item[key] = vim.fn.strcharpart(vim_item[key], 0, width - 1) .. "…"
+            end
+          end
+
+          return vim_item
+        end,
+      },
     },
   },
 }
