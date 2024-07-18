2e01c22e222a1f9206efd83a83429551ae0bf034
Ken Chu <chuwingyu@gmail.com>
2024-07-18 19:18:50 +0800

    fix(astronvim): disable toggleterm float window after overseer run

--- nvim-astronvim/.config/nvim-astronvim/lua/plugins/task-runner.lua
+++ nvim-astronvim/.config/nvim-astronvim/lua/plugins/task-runner.lua
@@ -1,31 +1,35 @@
 return {
   {
     "Zeioth/compiler.nvim",
-    cmd = { "CompilerOpen", "CompilerToggleResults", "CompilerRedo" },
-    -- stylua: ignore
     keys = {
-      { "<leader>::", "<cmd>CompilerOpen<cr>",          desc = "Compiler Open" },
-      { "<leader>:r", "<cmd>CompilerRedo<cr>",          desc = "Compiler Redo" },
-      { "<leader>:x", "<cmd>CompilerStop<cr>",          desc = "Compiler Stop" },
+      { "<leader>::", "<cmd>CompilerOpen<cr>", desc = "Compiler Open" },
+      { "<leader>:r", "<cmd>CompilerRedo<cr>", desc = "Compiler Redo" },
+      { "<leader>:x", "<cmd>CompilerStop<cr>", desc = "Compiler Stop" },
       { "<leader>:t", "<cmd>CompilerToggleResults<cr>", desc = "Compiler Toggle Results" },
     },
-    opts = {},
   },
+
   {
     "stevearc/overseer.nvim",
-    opts = {
-      task_list = {
-        direction = "bottom",
-        min_height = 0,
-        max_height = 25,
-        default_detail = 1,
-      },
-    },
+    opts = function(_, opts)
+      local astrocore = require "astrocore"
+      if astrocore.is_available "toggleterm.nvim" then
+        opts.strategy = {
+          "toggleterm",
+          open_on_start = false,
+        }
+      end
+    end,
   },
+
   {
     "AstroNvim/astrocore",
     ---@type AstroCoreOpts
     opts = {
+      rooter = {
+        autochdir = true,
+        notify = false,
+      },
       options = {
         g = {
           NODEJS_PACKAGE_MANAGER = "bun",
