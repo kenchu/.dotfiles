202916dc99e6145f89bb17f95c2299edc96204b2
Ken Chu <chuwingyu@gmail.com>
2024-07-22 15:44:51 +0800

    feat(astronvim): replace markview.nvim with markdown.nvim

--- nvim-astronvim/.config/nvim-astronvim/lua/plugins/markdown.lua
+++ nvim-astronvim/.config/nvim-astronvim/lua/plugins/markdown.lua
@@ -1,9 +1,10 @@
 return {
   {
-    "OXY2DEV/markview.nvim",
-    dependencies = {
-      "nvim-treesitter/nvim-treesitter",
-    },
-    ft = "markdown",
+    "MeanderingProgrammer/markdown.nvim",
+    name = "render-markdown", -- Only needed if you have another plugin named markdown.nvim
+    dependencies = { "nvim-treesitter/nvim-treesitter", "echasnovski/mini.nvim" }, -- if you use the mini.nvim suite
+    -- dependencies = { 'nvim-treesitter/nvim-treesitter', 'echasnovski/mini.icons' }, -- if you use standalone mini plugins
+    -- dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-tree/nvim-web-devicons' }, -- if you prefer nvim-web-devicons
+    config = function() require("render-markdown").setup {} end,
   },
 }
