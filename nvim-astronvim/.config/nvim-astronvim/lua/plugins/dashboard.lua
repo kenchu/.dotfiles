1eaae89ded0f64e2a5ed1e332a00379c5ca74ce3
Ken Chu <chuwingyu@gmail.com>
2025-01-03 01:35:04 +0800

    style(AstroNvim): dashboard shortcut keybinding

--- nvim-astronvim/.config/nvim-astronvim/lua/plugins/dashboard.lua
+++ nvim-astronvim/.config/nvim-astronvim/lua/plugins/dashboard.lua
@@ -2,17 +2,17 @@ return {
   "goolord/alpha-nvim",
   opts = function(_, opts)
     opts.section.buttons.val = {
-      opts.button("n", "  New File", "<cmd>ene <bar> startinsert<cr>"),
+      opts.button("n", "  New File", "<cmd>enew <bar> startinsert<cr>"),
       opts.button("f", "  Find File", "<cmd>Telescope find_files<cr>"),
       opts.button("o", "  Old File", "<cmd>Telescope oldfiles<cr>"),
-      opts.button("g", "  Find Text", "<cmd>Telescope live_grep<cr>"),
+      opts.button("w", "  Find Text", "<cmd>Telescope live_grep<cr>"),
       opts.button("p", "  Project", "<cmd>Telescope project<cr>"),
       opts.button("z", "  Zoxide", "<cmd>Telescope zoxide list<cr>"),
       opts.button("s", "  Load Session", "<cmd>lua require('resession').load()<cr>"),
       opts.button("l", "  Lazy", "<cmd>Lazy<cr>"),
       opts.button(
-        "c",
-        "  Configuration",
+        "a",
+        "  Astronvim Configuration",
         function() require("telescope.builtin").find_files { cwd = vim.fn.stdpath "config" } end
       ),
       opts.button("q", "  Quit Neovim", ":qa<CR>"),
