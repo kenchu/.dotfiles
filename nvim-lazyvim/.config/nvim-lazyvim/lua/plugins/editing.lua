83af3e6c7ebb746d7b93994623a25c8744228a7e
Ken Chu <chuwingyu@gmail.com>
2025-03-09 23:56:31 +0800

    feat(nvim-lazyvim): add some editing plugins

---
+++ nvim-lazyvim/.config/nvim-lazyvim/lua/plugins/editing.lua
@@ -0,0 +1,29 @@
+return {
+  {
+    "max397574/better-escape.nvim",
+    event = "InsertCharPre",
+    opts = {
+      mapping = { "jk", "kj" },
+      timeout = 300,
+    },
+  },
+
+  -- { "NStefan002/visual-surround.nvim", event = "VeryLazy", config = true },
+
+  -- {
+  --   "chrisgrieser/nvim-spider",
+  --   -- stylua: ignore
+  --   keys = {
+  --     { ",b", function() require("spider").motion("b") end, mode = { "n", "o", "x" } },
+  --     { ",e", function() require("spider").motion("e") end, mode = { "n", "o", "x" } },
+  --     { ",w", function() require("spider").motion("w") end, mode = { "n", "o", "x" } },
+  --     { ",ge", function() require("spider").motion("ge") end, mode = { "n", "o", "x" } },
+  --   },
+  -- },
+
+  {
+    "lambdalisue/suda.vim",
+    keys = { { "<leader>fs", "<cmd>SudaWrite<cr>", desc = "Write File (sudo)" } },
+    cmd = { "SudaRead", "SudaWrite" },
+  },
+}
