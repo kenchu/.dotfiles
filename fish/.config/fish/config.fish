9cb6374ee410265d6edbe737c120d7793c6338be
Ken Chu <chuwingyu@gmail.com>
2025-03-09 23:57:17 +0800

    style(fzf): remove catppuccin theme from fzf config

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -34,12 +34,12 @@ set -x LANG en_US.UTF-8
 set -x XDG_CONFIG_HOME $HOME/.config
 set -x JAVA_HOME $HOME/.asdf/installs/java/graalvm-22.3.1+java17
 # https://github.com/catppuccin/fzf
-set -x FZF_DEFAULT_OPTS "\
---color=bg+:#313244,bg:#1e1e2e,spinner:#f5e0dc,hl:#f38ba8 \
---color=fg:#cdd6f4,header:#f38ba8,info:#cba6f7,pointer:#f5e0dc \
---color=marker:#b4befe,fg+:#cdd6f4,prompt:#cba6f7,hl+:#f38ba8 \
---color=selected-bg:#45475a \
---multi"
+# set -x FZF_DEFAULT_OPTS "\
+# --color=bg+:#313244,bg:#1e1e2e,spinner:#f5e0dc,hl:#f38ba8 \
+# --color=fg:#cdd6f4,header:#f38ba8,info:#cba6f7,pointer:#f5e0dc \
+# --color=marker:#b4befe,fg+:#cdd6f4,prompt:#cba6f7,hl+:#f38ba8 \
+# --color=selected-bg:#45475a \
+# --multi"
 
 # set fish_user_paths
 fish_add_path $HOME/.asdf/shims
