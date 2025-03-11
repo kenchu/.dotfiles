f1211a5dc1e16e15165a9a31ae53be979ac3b4c7
Ken Chu <chuwingyu@gmail.com>
2025-03-11 20:04:01 +0800

    feat(nvim-lazyvim): add neovide options for meta key

--- nvim-lazyvim/.config/nvim-lazyvim/lua/config/options.lua
+++ nvim-lazyvim/.config/nvim-lazyvim/lua/config/options.lua
@@ -12,11 +12,14 @@ if vim.loop.os_uname().sysname == "Darwin" then
 else
   print("Not running on macOS")
   vim.o.guifont = "JetBrainsMono Nerd Font:h12"
-  vim.o.linespace = 4
+  vim.o.linespace = 0
 end
 
 if vim.g.neovide then
-  vim.g.neovide_transparency = 0.97
-  vim.g.neovide_hide_mouse_when_typing = true
+  vim.g.neovide_opacity = 0.0
+  vim.g.neovide_normal_opacity = 0.95
   vim.g.neovide_cursor_smooth_blink = true
+  vim.g.neovide_hide_mouse_when_typing = true
+  vim.g.neovide_input_ime = true
+  vim.g.neovide_input_macos_option_key_is_meta = "only_left"
 end
